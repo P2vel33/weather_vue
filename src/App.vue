02d4c0415399e1ace511a7beb0f4e34a5b6e9f96
Pavel <samasyt@mail.ru>
2025-04-07 12:11:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<script setup>
+import WeatherWidgets from "./components/WeatherWidgets.vue";
+</script>
+
+<template>
+  <div class="wheather">
+    <WeatherWidgets />
+    <WeatherWidgets />
+    <WeatherWidgets />
+    <WeatherWidgets />
+  </div>
+
+  <!-- <div class="div">
+    <div class="rectangle">
+      <p class="label1">19°</p>
+      <div class="image"></div>
+      <div class="label2">
+        <p class="label21">H:24° L:18°</p>
+        <p class="label22">Montreal, Canada</p>
+      </div>
+      <p class="label3">Partly Cloudy</p>
+    </div>
+  </div> -->
+</template>
+
+<style>
+* {
+  margin: 0%;
+}
+.wheather {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  border-radius: 44px;
+  box-shadow: 40px 60px 150px 0px rgba(59, 38, 123, 0.7);
+  background: linear-gradient(
+    136.55deg,
+    rgb(46, 51, 90) 1.538%,
+    rgb(28, 27, 51) 95.915%
+  );
+  padding: 10%;
+}
+/* .div {
+  position: relative;
+  width: 342px;
+  height: 184px;
+  left: 20px;
+}
+.rectangle {
+  position: absolute;
+  background-image: url("/public/Subtract.svg");
+  width: 339px;
+  height: 167px;
+  background-repeat: no-repeat;
+  top: 11px;
+  bottom: 1px;
+}
+.label1 {
+  position: absolute;
+  left: 20px;
+  top: 48px;
+  color: rgb(255, 255, 255);
+  font-family: SF Pro Display;
+  font-size: 64px;
+  font-weight: 400;
+  line-height: 41px;
+  letter-spacing: 0.37px;
+  text-align: left;
+}
+.label2 {
+  position: absolute;
+
+  width: 136px;
+  height: 41px;
+  left: 20px;
+  top: 113px;
+  display: flex;
+  flex-direction: column;
+  gap: 0px;
+  margin: 0;
+}
+.label3 {
+  position: absolute;
+  width: 81px;
+  height: 18px;
+  left: 237px;
+  top: 136px;
+  color: rgb(255, 255, 255);
+  font-family: SF Pro Text;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.08px;
+}
+.label21 {
+  color: rgba(235, 235, 245, 0.6);
+}
+
+.label22 {
+  color: white;
+}
+.image {
+  position: absolute;
+  width: 160px;
+  height: 160px;
+  left: 175px;
+  top: -30px;
+  background: url("/public/MoonCloudFastWind.svg");
+} */
+</style>
---
+++ src/components/WeatherWidgets.vue
@@ -0,0 +1,106 @@
+<script setup>
+const width = window.innerWidth;
+const height = window.innerHeight;
+</script>
+
+<template>
+  <div class="div">
+    <div class="rectangle">
+      <!-- <img src="/public/Subtract.svg" class="rectangle" /> -->
+      <p class="label1">19°</p>
+      <div class="image"></div>
+      <div class="label2">
+        <p class="label21">H:24° L:18°</p>
+        <p class="label22">Montreal, Canada</p>
+      </div>
+      <p class="label3">Partly Cloudy</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.div {
+  position: relative;
+  width: 100%;
+  height: 800px;
+  left: 20px;
+  align-items: center;
+}
+.rectangle {
+  position: absolute;
+  background-image: url("/public/Subtract.svg");
+  /* background-position: 90%; */
+  background-size: 99% 90%;
+  width: 99.1%;
+  height: 700px;
+  background-repeat: no-repeat;
+  top: 11px;
+  bottom: 1px;
+  display: flex;
+  justify-content: center;
+}
+.label1 {
+  position: absolute;
+  width: 27.7%;
+  height: 22.2%;
+  left: 5.8%;
+  top: 27.5%;
+  color: rgb(255, 255, 255);
+  font-family: SF Pro Display;
+  font-size: 11.3vw;
+  font-weight: 400;
+  line-height: 41px;
+  letter-spacing: 0.37px;
+  text-align: left;
+}
+.label2 {
+  position: absolute;
+  width: 39.7%;
+  height: 22.3%;
+  left: 5.8%;
+  top: 65%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 0px;
+  margin: 0;
+}
+
+.label3 {
+  position: absolute;
+  width: 24%;
+  height: 9.7%;
+  left: 69.2%;
+  top: 78.1%;
+  color: rgb(255, 255, 255);
+  font-family: SF Pro Text;
+  font-size: 2.71vw;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.08px;
+}
+.label21 {
+  color: rgba(235, 235, 245, 0.6);
+  height: 100%;
+  text-align: left;
+  vertical-align: text-top;
+  font-size: 2.71vw;
+}
+
+.label22 {
+  color: white;
+  height: 100%;
+  text-align: left;
+  font-size: 3.55vw;
+}
+.image {
+  position: absolute;
+  width: 46.7%;
+  height: 86.9%;
+  left: 52%;
+  top: -30px;
+  background-image: url("/public/MoonCloudFastWind.svg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+</style>
